2de284f702ef2ef61666b2c89a35e5bf657247ee
hlprojects <Pj6=zWB>
2015-07-21 17:51:51 +0900

    サンプル実装
    サンプル実装途中

---
+++ jsrensyu/Assets/Resources/Scripts/MailController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Linq;
+
+public class MailController : MonoBehaviour {
+
+    public GameObject MailList = null;
+    public UnityEngine.UI.Text ContentName = null;
+    public UnityEngine.UI.Text ContentTitle = null;
+
+    public static MailController Instance = null;
+
+    public MailController()
+    {
+        Instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject prefab = (GameObject)Resources.Load("Prefabs/MailItemButton");
+
+        XmlDocument xd = new XmlDocument();
+        using(System.IO.FileStream s = new System.IO.FileStream("happylagoon.xml",System.IO.FileMode.Open))
+        {
+            xd.Load(s);
+        }
+        foreach (XmlNode n in xd.DocumentElement.ChildNodes)
+        {
+            var o = Instantiate(prefab);
+            o.transform.parent = MailList.transform;
+            var c = o.GetComponent<MailItemButtonController>();
+            foreach (XmlAttribute a in n.Attributes)
+            {
+                if (a.Name == "Author") c.Name = a.Value;
+                if (a.Name == "Subject") c.Subject = a.Value;
+                if (a.Name == "Icon")
+                {
+                    try
+                    {
+                        var r = (UnityEngine.Texture2D)Resources.Load(string.Format("Textures/{0}", a.Value));
+                        UnityEngine.Sprite s = UnityEngine.Sprite.Create(r, new Rect(0, 0, r.width, r.height), new Vector2(0.5f, 0.5f));
+                        c.Icon = s;
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Debug.LogError(ex.ToString());
+                    }
+                }
+            }
+            Debug.Log(string.Format("{0}:{1}", c.Name, c.Subject));
+            foreach (XmlNode n2 in n.ChildNodes)
+            {
+            }
+        }
+
+        
+
+        //for (int i = 0; i < 15; i++)
+        //{
+        //    var o = Instantiate(prefab);
+        //    o.transform.parent = MailList.transform;
+        //    var c = o.GetComponent<MailItemButtonController>();
+        //    c.Name = string.Format("{0}item", i);
+        //    c.Subject = string.Format("{0}subject", i);
+        //}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetItem(MailItemButtonController mic)
+    {
+        ContentName.text = mic.Name;
+        ContentTitle.text = mic.Subject;
+    }
+
+}
---
+++ jsrensyu/Assets/Resources/Scripts/MailItemButtonController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MailItemButtonController : MonoBehaviour {
+
+    public string Name = "ひめ";
+    public string Subject = "聞いて聞いて";
+    public UnityEngine.Sprite Icon = null;
+
+    public UnityEngine.UI.Text NameField = null;
+    public UnityEngine.UI.Text SubjectField = null;
+    public UnityEngine.UI.Image IconObject = null;
+
+    // Use this for initialization
+	void Start () {
+        NameField.text = Name;
+        SubjectField.text = Subject;
+        IconObject.sprite = Icon;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Clicked()
+    {
+        var mc = MailController.Instance;
+        mc.SetItem(this);
+    }
+}
